8e22f68dab056ca79c50209c339082be039f757c
Stanley Pan <stanleypan.egre@gmail.com>
2017-10-09 21:09:52 +0900

    Can place ramps within the scene

--- RampUp/Controller/RampPlacerVC.swift
+++ RampUp/Controller/RampPlacerVC.swift
@@ -12,7 +12,11 @@ import ARKit
 
 class RampPlacerVC: UIViewController, ARSCNViewDelegate {
 
+    //  MARK: Outlets
     @IBOutlet var sceneView: ARSCNView!
+
+    //  MARK: Variables
+    var selectedRamp: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +28,8 @@ class RampPlacerVC: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/pipe.dae")!
+        let scene = SCNScene(named: "art.scnassets/main.scn")!
+        sceneView.autoenablesDefaultLighting = true
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -89,9 +94,27 @@ class RampPlacerVC: UIViewController, ARSCNViewDelegate {
     }
     
     func onRampSelected(_ rampName: String) {
-        
+        self.selectedRamp = rampName
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let results = sceneView.hitTest(touch.location(in: sceneView), types: [.featurePoint])
+        
+        guard let hitFeature = results.last else { return }
+        let hitTransform = SCNMatrix4(hitFeature.worldTransform)
+        let hitPosition = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43)
+        placeRamp(position: hitPosition)
+    }
+    
+    func placeRamp(position: SCNVector3) {
+        if let rampName = selectedRamp {
+            let ramp = Ramp.getRampForName(rampName: rampName)
+            ramp.position = position
+            ramp.scale = SCNVector3Make(0.01, 0.01, 0.01)
+            sceneView.scene.rootNode.addChildNode(ramp)
+        }
+    }
 }
 
 extension RampPlacerVC: UIPopoverPresentationControllerDelegate {
